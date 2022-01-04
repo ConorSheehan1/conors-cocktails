b3b910cd94cf38609470ac2fe45eecb9cf2d2586
Conor Sheehan <conor.sheehan.dev@gmail.com>
2022-01-04 13:22:50 +0000

    basic element-ui button

--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,10 @@
 <template>
-  <Tutorial />
+  <div id="app">
+    <el-button @click="visible = true">Button</el-button>
+    <el-dialog :visible.sync="visible" title="Hello world">
+      <p>Try Element</p>
+    </el-dialog>
+  </div>
 </template>
 
 <script lang="ts">
@@ -7,5 +12,10 @@ import Vue from 'vue'
 
 export default Vue.extend({
   name: 'IndexPage',
+  data() {
+    return {
+      visible: true,
+    }
+  }
 })
 </script>
