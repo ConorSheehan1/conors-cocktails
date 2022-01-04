a66923654f7795b38db2ee6fd3149c2106b8a29c
Conor Sheehan <conor.sheehan.dev@gmail.com>
2022-01-04 15:59:43 +0000

    basic espresso martini recipe and ingredients component

---
+++ src/.vuepress/components/ingredients.vue
@@ -0,0 +1,70 @@
+<template>
+  <div>
+    <select v-model="selectedFormat">
+      <option :key="i" v-for="format, i in formats" :value="format">{{format}}</option>
+    </select>
+    <ul>
+      <li :key="i" v-for="ingredient, i in formattedIngredients">
+        {{ ingredient.amount }} {{ ingredient.unit }} {{ ingredient.name }}
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+  export default {
+    data() {
+      return {
+        formats: ['parts', 'ml', 'oz'],
+        selectedFormat: 'parts',
+      }
+    },
+    props: {
+      ingredients: {
+        type: Array,
+      },
+    },
+    computed: {
+      formattedIngredients() {
+        return this.ingredients.map(this[`${this.selectedFormat}Format`]);
+      },
+      totalParts() {
+        return this.ingredients.reduce((acc, ingredient) => {
+          if (this.isPartsIngredient(ingredient)) {
+            acc += parseInt(ingredient.amount, 10)
+          }
+        }, 0)
+      }
+    },
+    methods: {
+      isPartsIngredient({ unit }) {
+        return unit.toLowerCase().includes('part')
+      },
+      // recipes are defined in parts, so just return original
+      partsFormat(ingredient) {
+        return ingredient;
+      },
+      // conversions source https://support.google.com/websearch/answer/3284611?hl=en-IE#unitconverter
+      mlFormat(ingredient) {
+        if (!this.isPartsIngredient(ingredient)) return ingredient;
+
+        return {
+          ...ingredient,
+          amount: Math.round(ingredient.amount * 45),
+          unit: 'ml',
+          // 1 jigger -> millileters 44.3603
+        }
+      },
+      ozFormat(ingredient) {
+        if (!this.isPartsIngredient(ingredient)) return ingredient;
+
+        return {
+          ...ingredient,
+          amount: Math.round(ingredient.amount * 1.6),
+          unit: 'oz'
+          // 1 jigger -> fliud ounces 1.56126
+        }
+      }
+    }
+  }
+</script>
--- src/cocktails/espresso-martini.md
+++ src/cocktails/espresso-martini.md
@@ -1,12 +1,43 @@
 # Espresso Martini
 
 ## Ingredients
-1. coffee
-2. khalua
-3. vanilla vodka
-4. maple syrup
+<ingredients
+  :ingredients="[
+    { 
+      name: 'coffee',
+      amount: '1',
+      unit: 'cup',
+    },
+    {
+      name: 'kahlua',
+      amount: '2',
+      unit: 'parts',
+    },
+    {
+      name: 'vanilla vodka',
+      amount: '1',
+      unit: 'part',
+    },
+    {
+      name: 'maple syrup',
+      amount: 'dash',
+      unit: 'to taste',
+    },
+  ]"
+/>
+
 
 ## Equipment
-1. cocktail shaker
+* cocktail shaker
+* martini glasses
 
 ## Recipe
+1. Make a cup of coffee and allow to cool.
+2. Add the coffee, kahlua, vanilla vodka, and mapel syrup to the cocktail shaker.
+3. Shake!
+
+### Notes
+* A coffee with lots of crema is best to get a frothy top. Regular instant coffee works ok though. The maple syrup will help with the froth. Adding a few teaspoons of sugar to the coffee while brewing can help too.
+* Once the coffee has brewed, add some ice cubes to speed up the cooling. 
+* Espresso martinis are often served with coffee bean garnishes, but I much prefer adding some chocolate powder the top!
+
