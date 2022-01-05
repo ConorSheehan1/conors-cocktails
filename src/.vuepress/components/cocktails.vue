35b6d0cb1e8b39bd7a29f4c2925967ee7e0cb9ce
Conor Sheehan <conor.sheehan.dev@gmail.com>
2022-01-05 12:40:01 +0000

    basic bootsrap table, but sort icon not appearing

--- src/.vuepress/components/cocktails.vue
+++ src/.vuepress/components/cocktails.vue
@@ -1,5 +1,16 @@
 <template>
   <div>
+    <b-table
+      :items="cocktailPages.map(v => v.frontmatter)"
+      :fields="tableFields"
+      :sort-by.sync="sortBy"
+      :sort-desc.sync="sortDesc"
+      sort-icon-left
+      responsive="sm"
+      striped
+      hover
+    >
+    </b-table>
     <div :key="i" v-for="(cocktailPage, i) in cocktailPages">
       <h3 :id="pageName(cocktailPage)">
         <a :href="cocktailPage.path">
@@ -19,12 +30,33 @@
 <script>
 // TODO: make searchable cocktail table
 export default {
+  data() {
+    return {
+      sortBy: 'title',
+      sortDesc: false,
+      tableFields: [
+        'title',
+        'ingredients',
+        'flavours',
+        'difficulty'
+      ]
+    }
+  },
   computed: {
     cocktailPages() {
       return this.$site.pages.filter((page) =>
         page.path.match("\/cocktails\/.+")
       );
     },
+    // tableData() {
+    //   return this.cocktailPages.map(page => {
+    //     const {  }
+    //     return {
+    //       ...page.frontmatter,
+
+    //     }
+    //   });
+    // },
   },
   methods: {
     pageName(page) {
--- src/.vuepress/enhanceApp.js
+++ src/.vuepress/enhanceApp.js
@@ -4,11 +4,19 @@
  * https://v1.vuepress.vuejs.org/guide/basic-config.html#app-level-enhancements
  */
 
+import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
+// Import Bootstrap an BootstrapVue CSS files (order is important)
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
+
 export default ({
-  Vue, // the version of Vue being used in the VuePress app
-  options, // the options for the root Vue instance
-  router, // the router instance for the app
-  siteData, // site metadata
+  Vue,
+  options,
+  router,
+  siteData
 }) => {
-  // ...apply enhancements for the site.
-};
+  // Make BootstrapVue available throughout your project
+  Vue.use(BootstrapVue)
+  // Optionally install the BootstrapVue icon components plugin
+  Vue.use(IconsPlugin)
+}
